dd2ab5d7c6eaf6e8c3c7758ea1c91458ced827f2
glenngalicia <glenn.f.g.a@gmail.com>
2026-01-01 13:48:24 -0600

    Initial Project with 2 files that explane inputs and strings into Python

---
+++ First_Project.py
@@ -0,0 +1,13 @@
+# Declaration Print
+print('Hello World "this is my first script in Python" ')
+print(500 + 55)
+
+# Strings y secuencias de escape \
+print('Hola' + ' ' + "Glenn")
+print("Caracter \"especial\"")
+print('Esta es una linea \nAqui otra linea')
+print('A\tB\tC\nD\tE\tF\nG\tH\tI')
+
+# Inputs
+print('Your name is: ' + input('Put your name: ') + ' ' + input('Put your last name: '))
+
