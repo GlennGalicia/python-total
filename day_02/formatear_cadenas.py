7492b9f97a360c78d3d4765ffccd0dda8ed3e223
glenngalicia <glenn.f.g.a@gmail.com>
2026-01-01 22:56:19 -0600

    feat: Add `formatear_cadenas.py` for string formatting examples

---
+++ day_02/formatear_cadenas.py
@@ -0,0 +1,10 @@
+# Format
+x = 10
+y = 20
+print('Mis numeros son {} y {}'.format(x, y))
+print('La suma de {} + {} es igual a: {}'.format(x, y, x+y))
+
+# Cadena Literal
+color = 'Blanco'
+matricula = 297
+print(f'El color de tu vehiculo es {color} y su matricula es {matricula}')
