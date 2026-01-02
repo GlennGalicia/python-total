49736a9f642ca5e0c42e09288ed50f474debf617
glenngalicia <glenn.f.g.a@gmail.com>
2026-01-01 22:55:34 -0600

    feat: Add `conversiones.py` for type conversion examples

---
+++ day_02/conversiones.py
@@ -0,0 +1,17 @@
+num1 = 20
+num2 = 30.5
+
+print(type(num1))
+print(type(num2))
+
+num3 = 5.5
+print(num3)
+print(type(num3))
+num4 = int(num3)
+print(num4)
+print(type(num4))
+
+edad = int(input('Escribe tu edad: '))
+print(edad + 1)
+print(type(edad))
+
