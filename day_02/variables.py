71efa844442e6fec7ecf76db389abedcdfa652a2
glenngalicia <glenn.f.g.a@gmail.com>
2026-01-01 22:52:02 -0600

    feat: Add `variables.py` with basic variable examples

---
+++ day_02/variables.py
@@ -0,0 +1,23 @@
+# variables tipo Strings
+name = "Glenn Aviña"
+print(name)
+
+name = "Rocio Aviña"
+print(name)
+
+# Variables tipo Int
+edad = 34
+edad2 = 59
+print(edad)
+print(edad + edad2)
+
+fullName = input('Escribe tu nombre completo: ')
+print('Hola, tu nombre completo es: ' + fullName)
+
+# Concatenar variables Strings
+firstName = "Fernando"
+lastName = "Aviña"
+fullName = firstName + " " + lastName
+print(fullName)
+
+
