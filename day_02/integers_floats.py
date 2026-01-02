de7f7963ec59e3c6762fe4a266838b6aa078c78d
glenngalicia <glenn.f.g.a@gmail.com>
2026-01-01 22:55:05 -0600

    feat: Introduce basic examples for integers, floats, and user input

---
+++ day_02/integers_floats.py
@@ -0,0 +1,17 @@
+# Integers
+mi_numero = 33 + 1
+print(mi_numero + mi_numero)
+print(type(mi_numero))
+
+# Floats
+mi_flotante = 5.8
+print(mi_flotante + mi_flotante)
+print(type(mi_flotante))
+
+edad = input("Ingresa tu edad: ")
+print('Tu edad es: ' + edad)
+print(type(edad))
+
+num1 = 7.5
+num2 = 2.5
+print(type(num1+num2))
