422cd565795a4a3f295610895f412714ea073888
glenngalicia <glenn.f.g.a@gmail.com>
2026-01-03 15:37:22 -0600

    feat: Demonstrate Python string properties

---
+++ day_3/propiedades.py
@@ -0,0 +1,19 @@
+# Concatenaciòn de cadenas
+nom1 = 'Gle'
+nom2 = 'nn'
+print(nom1 + nom2)
+
+# Multiplicar cadenas
+print(nom1 * 10)
+
+# Imprimir texto con saltos de linea sin secuencia de escape
+poema = '''Mil pequeños peses blancos
+como si hirviera
+el color del agua'''
+print(poema)
+
+# Buscar palabra en un texto
+print('agua' in poema)
+
+# Tamaño de una cadena
+print(len(poema))
