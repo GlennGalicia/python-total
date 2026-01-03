0bf2e24ade7b9971fd9b040b81416fa3148bd82a
glenngalicia <glenn.f.g.a@gmail.com>
2026-01-03 15:37:21 -0600

    feat: Introduce boolean data types and comparison operators

---
+++ day_3/booleanos.py
@@ -0,0 +1,17 @@
+# Declaración de Booleanos
+var1 = True
+var2 = False
+print(type(var1))
+print(var1)
+
+# Operadores de comparación
+comp1 = 5 > 3 + 2
+print(f'La comparación de 5 > 3+2 es: {comp1}')
+
+comp2 = 5 == 3 + 2
+print(f'La comparación de 5 = 3+2 es: {comp2}')
+
+# Booleano en listas
+mi_lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+control = 11 in mi_lista
+print(f'El número 11 se encuentra en la lista? : {control}')
