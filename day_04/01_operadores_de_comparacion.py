66c5fed97eb8837fa79e0644dbef1a14325e15d1
glenngalicia <glenn.f.g.a@gmail.com>
2026-01-04 22:47:38 -0600

    feat(day04): Add comparison operators examples

---
+++ day_04/01_operadores_de_comparacion.py
@@ -0,0 +1,11 @@
+# Comparaciòn de Int en igualdad
+mi_bool = 25 == 10
+print(f'La comparación de 25 = 10 es: {mi_bool}')
+
+# Comparación de Strings en igualdad
+mi_bool_string = 'blaco' == 'negro'
+print(f'La comparación de \"blaco\" = \"negro\" es: {mi_bool_string}')
+
+# Comparación de Int en desigualdad
+mi_comparacion_int = 10 > 7
+print(f'La comparación de 10 > 7 es: {mi_comparacion_int}')
