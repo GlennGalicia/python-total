ef7675cc11da3c203c477b2afad033a7dcf28d92
glenngalicia <glenn.f.g.a@gmail.com>
2026-01-10 00:11:23 -0600

    refactor: Cleanup `devolver_distintos` in exercise 01

--- day_05/excercises/01_devolver_distintos.py
+++ day_05/excercises/01_devolver_distintos.py
@@ -1,7 +1,5 @@
 def devolver_distintos(a, b, c):
     lista = [a, b, c]
-    suma = 0
-    suma += sum(lista)
 
     if suma > 15:
         return max(lista)
@@ -13,6 +11,5 @@ def devolver_distintos(a, b, c):
 
 
 # Ejemplos de uso:
-print(devolver_distintos(5, 6, 7))  # suma = 18 → 7
 print(devolver_distintos(1, 2, 3))  # suma = 6 → 1
 print(devolver_distintos(5, 4, 6))  # suma = 15 → 5
