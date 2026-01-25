3e4ce9950d00205a2c89217eace1f7384ea521f4
glenngalicia <glenn.f.g.a@gmail.com>
2026-01-24 21:32:57 -0600

    teach: The receipt button now generates a receipt

--- day_12/mi_restaurante.py
+++ day_12/mi_restaurante.py
@@ -1,9 +1,12 @@
 from tkinter import *
+import datetime
+import random
 
 operador = ''
 precios_comida = [1.32, 1.65, 2.31, 3.22, 1.22, 1.99, 2.05, 2.65]
 precios_bebida = [0.25, 0.99, 1.21, 1.54, 1.08, 1.10, 2.00, 1.58]
 precios_postres = [1.54, 1.68, 1.32, 1.97, 2.55, 2.14, 1.94, 1.74]
+
 
 def click_boton(num):
     global operador
@@ -17,12 +20,14 @@ def borrar():
     operador = ''
     visor_calculadora.delete(0, END)
 
+
 def obtener_resultado():
     global operador
     resultado = str(eval(operador))
     visor_calculadora.delete(0, END)
     visor_calculadora.insert(0, resultado)
     operador = ''
+
 
 def revisar_check():
     x = 0
@@ -61,6 +66,7 @@ def revisar_check():
             texto_postre[z].set('0')
         z += 1
 
+
 def total():
     suma_total_comida = 0
     indice = 0
@@ -91,6 +97,50 @@ def total():
     var_subtotal.set(f'$ {round(suma_total, 2)}')
     var_impuesto.set(f'$ {round(impuestos, 2)}')
     var_total.set(f'$ {round(total, 2)}')
+
+
+def recibo():
+    texto_recibo.delete(1.0, END)
+    numero_recibo = f'N# - {random.randint(1000, 9999)}'
+    fecha = datetime.datetime.now()
+    fecha_recibo = f'{fecha.day}/{fecha.month}/{fecha.year} - {fecha.hour}:{fecha.minute}'
+    texto_recibo.insert(END, f'Datos:\t{numero_recibo}\t\t{fecha_recibo}\n')
+    texto_recibo.insert(END, f'*' * 61 + '\n')
+    texto_recibo.insert(END, 'Items\t\tCant.\tCosto Items\n')
+    texto_recibo.insert(END, f'-' * 61 + '\n')
+
+    x = 0
+    for comida in texto_comida:
+        if comida.get() != '0':
+            texto_recibo.insert(END, f'{lista_comidas[x]}\t\t{comida.get()}\t'
+                                     f'${int(comida.get()) * precios_comida[x]}\n')
+        x += 1
+
+    x = 0
+    for bebida in texto_bebida:
+        if bebida.get() != '0':
+            texto_recibo.insert(END, f'{lista_bebidas[x]}\t\t{bebida.get()}\t'
+                                     f'${int(bebida.get()) * precios_bebida[x]}\n')
+        x += 1
+
+    x = 0
+    for postre in texto_postre:
+        if postre.get() != '0':
+            texto_recibo.insert(END, f'{lista_postres[x]}\t\t{postre.get()}\t'
+                                     f'${int(postre.get()) * precios_postres[x]}\n')
+        x += 1
+
+    texto_recibo.insert(END, f'-' * 61 + '\n')
+    texto_recibo.insert(END, f'Costo de la Comida: \t\t\t{var_costo_comida.get()}\n')
+    texto_recibo.insert(END, f'Costo de la Bebida: \t\t\t{var_costo_bebida.get()}\n')
+    texto_recibo.insert(END, f'Costo de la Postre: \t\t\t{var_costo_postre.get()}\n')
+    texto_recibo.insert(END, f'-' * 61 + '\n')
+    texto_recibo.insert(END, f'Subtotal: \t\t\t{var_subtotal.get()}\n')
+    texto_recibo.insert(END, f'Impuestos: \t\t\t{var_impuesto.get()}\n')
+    texto_recibo.insert(END, f'Titak: \t\t\t{var_total.get()}\n')
+    texto_recibo.insert(END, f'-' * 61 + '\n')
+    texto_recibo.insert(END,'Lo esperamos pronto')
+
 
 # Crear instancia
 aplicacion = Tk()
@@ -378,6 +428,7 @@ for boton in lista_botones:
     columnas += 1
 
 botones_creados[0].config(command=total)
+botones_creados[1].config(command=recibo)
 
 # area de recibo
 texto_recibo = Text(panel_recibo,
