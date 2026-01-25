0e6811ba3d120ff1a508390a68073f32c15c4259
glenngalicia <glenn.f.g.a@gmail.com>
2026-01-24 23:57:05 -0600

    teach: transform voice into text

--- day_13/asistente_virtual.py
+++ day_13/asistente_virtual.py
@@ -7,3 +7,53 @@ import webbrowser
 import datetime
 import wikipedia
 
+
+# escuchar microfono y devolver el audio como texto
+def transformar_audio_en_texto():
+    # almacenar el recognizer en una valiable
+    r = sr.Recognizer()
+
+    # configurar microfono
+    with sr.Microphone() as source:
+
+        # tiempo de espera
+        r.pause_threshold = 0.8
+
+        # informar comenzo la grabación
+        print("Ya puedes hablar...")
+
+        # guardar audio
+        audio = r.listen(source)
+
+        try:
+            # Buscar en google lo que escucho
+            pedido = r.recognize_google(audio, language='es-ES')
+
+            # Prueba de que pudo ingresar y transformar
+            print(f'Dijiste: {pedido}')
+
+            # devolver a pedido
+            return pedido
+        # No comprende el audio
+        except sr.UnknownValueError:
+
+            # No comprendio el audio
+            print("No se puede hablar....")
+
+            return 'Sigo esperando 1'
+
+        # en caso de no resolver el pedido
+        except sr.RequestError:
+
+            # No comprendio el audio
+            print("ups, no hay servicio....")
+
+            return 'Sigo esperando 2'
+
+        # errores inesperados
+        except Exception as e:
+            # Mostrar el error específico
+            print(f"Ups algo ha salido mal.... Error: {type(e).__name__}: {e}")
+            return 'Sigo esperando'
+
+transformar_audio_en_texto()
