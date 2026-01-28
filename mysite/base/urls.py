f523fac6bc97caf93afafa038bdddc6e7d5fc0bf
glenngalicia <glenn.f.g.a@gmail.com>
2026-01-27 18:54:04 -0600

    teach: Config detail view - base/urls.py > create the path for the tarea detail view - base/views.py > create class to the tarea detail, includes template name - templates > add the html view

--- mysite/base/urls.py
+++ mysite/base/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
 from .views import ListaPendientes
+from .views import DetalleTarea
 
-urlpatterns = [path('',ListaPendientes.as_view(),name='pendientes')]
+urlpatterns = [path('',ListaPendientes.as_view(),name='pendientes'),
+               path('tarea/<int:pk>',DetalleTarea.as_view(),name='tarea')]
