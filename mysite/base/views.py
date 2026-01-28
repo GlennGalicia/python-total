2f6b00e84c22b8694c43bc60e8798703a5a12797
glenngalicia <glenn.f.g.a@gmail.com>
2026-01-27 22:38:25 -0600

    teach: show specific information about user - class ListaPendientes now has a method to get context data - class CrearTarea implements a method for form validation - class EditarTarea now can edit fields titulo, descripcion, completo

--- mysite/base/views.py
+++ mysite/base/views.py
@@ -23,6 +23,12 @@ class ListaPendientes(LoginRequiredMixin, ListView):
     model = Tarea
     context_object_name = 'tareas'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tareas'] = context['tareas'].filter(usuario=self.request.user)
+        context['count'] = context['tareas'].filter(completo=False).count()
+        return context
+
 
 class DetalleTarea(LoginRequiredMixin, DetailView):
     model = Tarea
@@ -32,13 +38,17 @@ class DetalleTarea(LoginRequiredMixin, DetailView):
 
 class CrearTarea(LoginRequiredMixin, CreateView):
     model = Tarea
-    fields = '__all__'
+    fields = ['titulo','descripcion', 'completo']
     success_url = reverse_lazy('tareas')
+
+    def form_valid(self, form):
+        form.instance.usuario = self.request.user
+        return super(CrearTarea, self).form_valid(form)
 
 
 class EditarTarea(LoginRequiredMixin, UpdateView):
     model = Tarea
-    fields = '__all__'
+    fields = ['titulo','descripcion', 'completo']
     success_url = reverse_lazy('tareas')
 
 
