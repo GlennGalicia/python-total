0c1e7aa218d814470de4a0e85f92ed148f3885ab
glenngalicia <glenn.f.g.a@gmail.com>
2026-01-27 19:20:30 -0600

    teach: Create new task View and link from the home - templates/base/tarea_list.html add an link to create task - templates/base/tarea_form.html create view to fill a form - base/urls.py add the route to create a task - base/views.py create class detailtask and redirect to home

--- mysite/base/views.py
+++ mysite/base/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+
 from .models import Tarea
 
 # Create your views here.
@@ -12,3 +15,8 @@ class DetalleTarea(DetailView):
     model = Tarea
     context_object_name = 'tarea'
     template_name = 'base/tarea.html'
+
+class CrearTarea(CreateView):
+    model = Tarea
+    fields = '__all__'
+    success_url = reverse_lazy('tareas')
